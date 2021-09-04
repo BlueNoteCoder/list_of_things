6d070b7f8240948666b891ed579f224e92416afe
sarchie14 <spencer.archibald05@gmail.com>
2021-09-04 14:48:54 -0500

    Add function to view all entries in the db. Also add input for user to select that option

--- main.py
+++ main.py
@@ -1,13 +1,41 @@
+from db_util import DBUtil
 from utilities import Utilities
-import random
 from menu import Menu
-# Press the green button in the gutter to run the script.
-#if __name__ == '__main__':
+from menu import spaces
 
-books = [{'Author': 'Ben', 'Title': 'Dead'}, {'Author': 'Stu', 'Title': 'Alive'}]
+# TODO: Add comments as necessary
+# TODO: Create a function to make list of books appear neat
+menu = Menu()
+utilities = Utilities()
+db = DBUtil()
 
-gen = Utilities()
-gen.add_book('Poulson', 'Blind', books)
+books_read = []
+user_choices = {1: menu.list_books_page, 2: menu.add_book_prompt}
 
-print(books)
 
+def list_all_entries(user_selection, db):
+    user_choices[user_input]()
+    books = utilities.get_all_book_info(db)
+    num_spaces = spaces()
+
+    if books:
+        for book in books:
+            print(' ' + book[0] + ' ' * (num_spaces - (len(book[0]) + 2)) + book[1])
+    else:
+        print('There are no books stored.\nPerhaps some reading is in store')
+
+
+if __name__ == '__main__':
+
+    menu.print_main_menu()
+
+    while True:
+
+        user_input = int(input())
+
+        if user_input == 1:
+            list_all_entries(user_input, db)
+
+        elif user_input == 2:
+            book_title, author = user_choices[user_input]()
+            utilities.add_book(book_title, author, db)
