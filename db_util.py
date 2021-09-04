32b0b06eb5023267b54221c4134b18a23095f060
sarchie14 <spencer.archibald05@gmail.com>
2021-09-03 22:56:50 -0500

    Create Functions to add book/create table in db

--- db_util.py
+++ db_util.py
@@ -6,6 +6,8 @@ def create_connection(file_name):
     connection = None
 
     try:
+        print('Establishing a connection')
+
         connection = sqlite3.connect(file_name)
     except Error as e:
         print(e)
@@ -19,22 +21,57 @@ def create_connection(file_name):
 # TODO: Create a function to make the necessary tables if needed
 
 class DBUtil:
-    file_extension = '.sqlite'
+    """DBUtil class is written for sqlite
+    """
+
+    # Specify what extension you want the db to be
+    file_extension = '.db'
 
     def __init__(self, *args):
+
+        """ When DBUtil is initialized, it will look for an argument to
+        name the db file.
+        If no argument is provided, then it will assign the file name.
+        """
+
+        self.book_table = 'books_read'
+
         if len(args) == 0:
             self.db_file_name = self.set_db_file_name()
 
         elif len(args) == 1:
             self.db_file_name = args[0] + DBUtil.file_extension
 
-        create_connection(self.db_file_name)
+        self.conn = create_connection(self.db_file_name)
+
+        self.create_book_table()
 
     def get_db_file_name(self):
-        print(self.db_file_name)
+        return self.db_file_name
 
     def set_db_file_name(self):
         # Returns the name of the db file
-        return 'book_db' + DBUtil.file_extension
+        return 'books_read_db' + DBUtil.file_extension
 
-    # def add_book_to_db(self, title, author):
+    def create_book_table(self):
+        table_stmt = """CREATE TABLE IF NOT EXISTS {table_name} (
+                            book_title TEXT NOT NULL,
+                            author_name TEXT NOT NULL); """.format(table_name=self.book_table)
+
+        print('Creating Database')
+
+        cursor = self.conn.cursor()
+        cursor.execute(table_stmt)
+
+        print('Database Created')
+
+    def add_book_to_db(self, title, author):
+        stmt = """INSERT INTO {table_name}(book_title, author_name)
+                    VALUES(?,?) """.format(table_name=self.book_table)
+        info = (title, author)
+        cursor = self.conn.cursor()
+
+        print('Adding book to db')
+        cursor.execute(stmt, info)
+        self.conn.commit()
+        print('Added book to database')
