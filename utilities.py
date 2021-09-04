bd617707e4709c5e2becc2557b9d3bae1dee8965
sarchie14 <spencer.archibald05@gmail.com>
2021-09-04 14:47:57 -0500

    Add function for obtaining the entries from db_util

--- utilities.py
+++ utilities.py
@@ -2,5 +2,11 @@ class Utilities:
     def __init__(self):
         print('Utilities created')
 
-    def add_book(self, author, title, bookList):
-        bookList.append({'Author': author, 'Title': title})
+    def add_book(self, title, author, db):
+
+        db.add_book_to_db(title, author)
+        print('Added Book!')
+        # Maybe log that the book has been added.
+
+    def get_all_book_info(self, db):
+        return db.get_entries()
