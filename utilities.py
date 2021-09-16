4a9bc27dcbf41f58a66287a4eac973b69ddaf0b0
sarchie14 <spencerarchibald@Spencers-MacBook-Pro.local>
2021-09-15 21:37:15 -0500

    Add function to return num of books in the database

--- utilities.py
+++ utilities.py
@@ -2,11 +2,23 @@ class Utilities:
     def __init__(self):
         print('Utilities created')
 
+    # Returns the number of books in a database
+    def count_num_of_books_in_db(self, db):
+        books = db.get_entries()
+        num_of_books = 0
+
+        for book in books:
+            num_of_books += 1
+
+        return num_of_books
+
+    def get_all_book_info(self, db):
+        return db.get_entries()
+
     def add_book(self, title, author, db):
 
         db.add_book_to_db(title, author)
         print('Added Book!')
         # Maybe log that the book has been added.
 
-    def get_all_book_info(self, db):
-        return db.get_entries()
+# if __name__ == '__main__':
