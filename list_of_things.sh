29a17cb60f0fb10547ef76b01fd732d83bc4d2da
sarchie14 <spencer.archibald05@gmail.com>
2021-11-23 15:09:18 -0600

    Script now checks if there is a path for python3. If not then it will exit

--- list_of_things.sh
+++ list_of_things.sh
@@ -3,6 +3,8 @@
 # Find python3
 PYTHON_TYPE=$(which python3)
 
+[ -z "$PYTHON_TYPE" ] && { echo "Error: Python3 is not installed. Please install to use this script"; exit 1; }
+
 chmod +x main.py
 $PYTHON_TYPE main.py 
 
