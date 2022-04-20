4451b4fcd07612d47ee5649099d366bc9a3f3de7
Spencer Archibald <spencerarchibald@Spencers-MacBook-Pro.local>
2022-04-19 19:59:47 -0500

    Placed command to set terminal window size

--- list_of_things.sh
+++ list_of_things.sh
@@ -1,9 +1,11 @@
-#!/usr/bin/sh
+#!/bin/sh
 
 # Find python3
 PYTHON_TYPE=$(which python3)
 
 [ -z "$PYTHON_TYPE" ] && { echo "Error: Python3 is not installed. Please install to use this script"; exit 1; }
+
+printf '\e[8;50;150t'
 
 chmod +x main.py
 $PYTHON_TYPE main.py 
